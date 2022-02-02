20ab8cd4ddf7cee2b4acdf061643837f9de3a564
moradalkhatib <muradalkhateb@icloud.com>
2022-02-02 20:27:01 +0200

    edit login and register functions by use another hashpassword lib and test them

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,78 +1,83 @@
-const SellerModel  = require('../models/sellerModule')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const SellerModel = require("../models/sellerModule");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const hashPass = require("password-hash");
 
 
 const register = (req, res, next) => {
-    // function to register a new seller.
-    
-    bcrypt.hash(req.body.password, 10, function(err, hashedPass) {
-        if(err) {
-             res.json({
-                 error: err
-             })
-        }
-    })
-    let seller = new SellerModel ({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-
-        image: req.body.image,
-        service: req.body.service,
-        isSeller:req.body.isSeller
-    })
-    seller.save()
-    .then(seller =>{
-        res.json({
-            message : "Seller Added Successfully!"
-        })
-    }).catch(err=>{
-        res.json({
-            message : "An error"
-        }) 
-    })
-}
-
-const login = (req,res,next)=>{
-    // function to login exists seller.
-    var email = req.body.email;
-    var password = req.body.password;
-    
-    SellerModel.findOne({$or: [{email:email},{phone:email}]})
-    .then(user=>{
-        if(user){
-            bcrypt.compare(password,user.password,function(err,result){
-                if(err){
-                    res.json({error:err})
-                }
-                if(result){
-                    let token = jwt.sign({name: user.name}, 'verySecretValue', {expiresIn: '1h'})
-                    res.json({message:"Login Successful!",
-                    token
-                })
-                }
-                else{
-                    res.json({message:'password does not match!'})
-                }
-            })
-
-        }else{
-            res.json({message:"No user found"})
-        }
-    })
-    
+  // function to register a new seller.
+  let pass = hashPass.generate(req.body.password);
+  bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
+    if (err) {
+      res.json({
+        error: err,
+      });
     }
+  });
+  let seller = new SellerModel({
+    name: req.body.name,
+    email: req.body.email,
+    password: pass,
 
-const allSeller = (req , res )=>{
+    image: req.body.image,
+    service: req.body.service,
+    isSeller: req.body.isSeller,
+  });
+  seller
+    .save()
+    .then((seller) => {
+      res.json({
+        message: "Seller Added Successfully!",
+      });
+    })
+    .catch((err) => {
+      res.json({
+        messageError: err,
+      });
+    });
+};
 
-    SellerModel.find().then(response=>{
-        res.json({
-            response
-        })
-    }).catch(err=>{res.json({message:"An Error"})})
-}
+const login = (req, res, next) => {
+  // function to login exists seller.
+  var email = req.body.email;
+  var password = req.body.password;
 
-module.exports = {register,login,allSeller}
+  SellerModel.findOne({ email })
+    .then((user) => {
+      if (user) {
+        if (hashPass.verify(password, user.password)) {
+          let token = jwt.sign(
+            { name: user.name, isSeller: user.isSeller },
+            "verySecretValue",
+            {
+              expiresIn: "1h",
+            }
+          );
+          res.json({ message: "Login Successful!", token });
+        } else {
+          res.json("password does not match!");
+        }
+      } else {
+        res.json({ message: "No user found" });
+      }
+    })
+    .catch((err) => {
+      res.json({ messageError: err });
+    });
+};
+
+const allSeller = (req, res) => {
+  SellerModel.find()
+    .then((response) => {
+      res.json({
+        response,
+      });
+    })
+    .catch((err) => {
+      res.json({ message: "An Error" });
+    });
+};
+
+module.exports = { register, login, allSeller };
 
 
