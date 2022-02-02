258dacb8597d4e75d3f38dc06154879d34fdf46b
moradalkhatib <muradalkhateb@icloud.com>
2022-02-02 16:46:02 +0200

    create login function and finished two end points api/register and api/login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 
 
 const register = (req, res, next) => {
+    // function to register a new seller.
+    
     bcrypt.hash(req.body.password, 10, function(err, hashedPass) {
         if(err) {
              res.json({
@@ -32,4 +34,45 @@ const register = (req, res, next) => {
     })
 }
 
-module.exports = {register}
+const login = (req,res,next)=>{
+    // function to login exists seller.
+    var email = req.body.email;
+    var password = req.body.password;
+    
+    SellerModel.findOne({$or: [{email:email},{phone:email}]})
+    .then(user=>{
+        if(user){
+            bcrypt.compare(password,user.password,function(err,result){
+                if(err){
+                    res.json({error:err})
+                }
+                if(result){
+                    let token = jwt.sign({name: user.name}, 'verySecretValue', {expiresIn: '1h'})
+                    res.json({message:"Login Successful!",
+                    token
+                })
+                }
+                else{
+                    res.json({message:'password does not match!'})
+                }
+            })
+
+        }else{
+            res.json({message:"No user found"})
+        }
+    })
+    
+    }
+
+const allSeller = (req , res )=>{
+
+    SellerModel.find().then(response=>{
+        res.json({
+            response
+        })
+    }).catch(err=>{res.json({message:"An Error"})})
+}
+
+module.exports = {register,login}
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 // for register a new seller.
-router.post('/register',AuthController.register)
+router.post('/register',AuthController.register);
+
+// for login exists seller.
+router.post('/login',AuthController.login);
 
 module.exports = router
